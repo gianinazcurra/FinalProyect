74ce08ea367799ed8a97db90d62b43764d4af321
gloriamburgos <99819841+gloriamburgos@users.noreply.github.com>
2022-06-01 19:25:03 -0300

    agregue repositorio y servicio compra

---
+++ src/main/java/com/vineyarg/demo/servicios/CompraServicio.java
@@ -0,0 +1,78 @@
+
+package com.vineyarg.demo.servicios;
+
+import com.vineyarg.demo.entidades.Compra;
+import com.vineyarg.demo.entidades.Producto;
+import com.vineyarg.demo.entidades.Usuario;
+import com.vineyarg.demo.errores.Excepcion;
+import com.vineyarg.demo.repositorios.CompraRepositorio;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CompraServicio {
+    @Autowired
+    private CompraRepositorio compraRepositorio;
+    
+    @Transactional
+    public void crearCompra(Integer cantidad, Usuario usuario, List<Producto> listaProductos, Date fechaCompra, Double montoFinal, String direccionEnvio) throws Excepcion{
+        validar(cantidad, usuario, listaProductos, fechaCompra, montoFinal, direccionEnvio);
+        
+        Compra compra = new Compra();
+        compra.setCantidad(cantidad);
+        compra.setDireccionEnvio(direccionEnvio);
+        compra.setFechaCompra(fechaCompra);
+        compra.setListaProductos(listaProductos);
+        compra.setMontoFinal(montoFinal);
+        compra.setUsuario(usuario);
+        compraRepositorio.save(compra);
+    }
+    
+    @Transactional
+    public void quitarProducto(List<Producto> listaProductos, String id) throws Excepcion{
+        
+        
+    }
+    @Transactional
+    public void agregarProducto(List<Producto> listaProductos, String id) throws Excepcion{
+        
+        
+    }
+    
+    @Transactional
+    public void eliminarCompra(String id)throws Excepcion{
+        Optional<Compra> optional = compraRepositorio.findById(id);
+
+        if (optional.isPresent()) {
+            compraRepositorio.delete(optional.get());
+        }
+    }
+    
+    public void validar (Integer cantidad, Usuario usuario, List<Producto> listaProductos, Date fechaCompra, Double montoFinal, String direccionEnvio) throws Excepcion{
+       
+        if (cantidad == null || cantidad.toString().trim().isEmpty()) {
+            throw new Excepcion("La cantidad no puede ser nula");
+        }
+        if (usuario == null || usuario.toString().trim().isEmpty()) {
+            throw new Excepcion("El usuario no puede ser nulo");
+        }
+        if (listaProductos == null || listaProductos.toString().trim().isEmpty()) {
+            throw new Excepcion("Este valor no puede ser nulo");
+        }
+        if (fechaCompra == null || fechaCompra.toString().trim().isEmpty()) {
+            throw new Excepcion("Este valor no puede ser nulo");
+        }
+        if (montoFinal == null || montoFinal.toString().trim().isEmpty()) {
+            throw new Excepcion("Este valor no puede ser nulo");
+        }
+        if (direccionEnvio == null || direccionEnvio.trim().isEmpty()) {
+            throw new Excepcion("Este valor no puede ser nulo");
+        }
+    }
+
+}
